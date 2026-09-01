a6a6c3f497d37dde0488629a5399ecbda3c2a112
kayalvizhi <madhukayal88@gmail.com>
2026-09-01 18:20:59 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
-import resumeReducer from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
+import resumeReducer from '../reducers/resumeReducer';
 
-const store = createStore(resumeReducer);
+const store = configureStore({
+  reducer: {
+    resume: resumeReducer
+  }
+});
 
 export default store;
