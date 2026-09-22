710a3ac9c33b382581f36a511c449d017810cf8d
kayalvizhi <madhukayal88@gmail.com>
2026-09-22 14:22:14 +0530

    Delete src/redux directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from "redux";
-import { resumeReducer } from "./reducer";
-
-const store = createStore(
-  resumeReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-export default store;
